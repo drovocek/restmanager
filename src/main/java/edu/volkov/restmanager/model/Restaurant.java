ff98da2a413a789aca7af64cd64588236b706a6e
drovocek <mycodepg@gmail.com>
2020-11-17 16:15:36 +0300

    add base Restaurant model

---
+++ src/main/java/edu/volkov/restmanager/model/Restaurant.java
@@ -0,0 +1,44 @@
+package edu.volkov.restmanager.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "restaurant", uniqueConstraints = {@UniqueConstraint(name = "restaurants_unique_name_idx", columnNames = "name")})
+public class Restaurant extends AbstractNamedEntity {
+
+    @NotBlank
+    @Size(min = 5, max = 200)
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @NotBlank
+    @Pattern(regexp = "^(\\+\\d{1}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$")
+    @Size(max = 20)
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    public Restaurant(Integer id, String name, String address, String phone) {
+        super(id, name);
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public Restaurant(Restaurant restaurant) {
+        this(restaurant.getId(), restaurant.getName(), restaurant.getAddress(), restaurant.getPhone());
+    }
+}
